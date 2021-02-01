b5b39fbb750ce7ab176c5cdb480dd1338cb3b3ca
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-01 18:55:11 +0700

    update3 lab1_6

--- lab1_6.c
+++ lab1_6.c
@@ -4,42 +4,45 @@ int main()
 	unsigned int n,m;
 	unsigned long int t;
 	char s,ans;
-	scanf("%d %d %d",&n,&m,&t);
+	scanf("%u %u %lu",&n,&m,&t);
 	fflush(stdin);
 	scanf("%c",&s);
-	if(s='L')
+	if(n>0&&m>0&&t>0)
 	{
-		while(1)
+		if(s='L')
 		{
-			t-=n;
-			if(t<=0)
+			while(t>0)
 			{
-				ans='L';
-				break;
-			}
-			t-=m;
-			if(t<=0)
-			{
-				ans='R';
-				break;
+				t=t-n;
+				if(t<=0)
+				{
+					ans='L';
+					break;
+				}
+				t=t-m;
+				if(t<=0)
+				{
+					ans='R';
+					break;
+				}
 			}
 		}
-	}
-	else if(s='R')
-	{
-		while(1)
+		else if(s='R')
 		{
-			t-=m;
-			if(t<=0)
+			while(t>0)
 			{
-				ans='R';
-				break;
-			}
-			t-=n;
-			if(t<=0)
-			{
-				ans='L';
-				break;
+				t=t-m;
+				if(t<=0)
+				{
+					ans='R';
+					break;
+				}
+				t=t-n;
+				if(t<=0)
+				{
+					ans='L';
+					break;
+				}
 			}
 		}
 	}
