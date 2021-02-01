b488770b7dab2114bc654752c3d0a57b6030b07a
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-01 18:09:19 +0700

    Update lab1_3

--- lab1_3.c
+++ lab1_3.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main()
 {
-	unsigned long n;
-	scanf ("%d",&n);
-	printf("%d %d",n%3,n%11);
+	unsigned long long int n;
+	int x1,x2;
+	scanf ("%llu",&n);
+	x1=n%3;
+	x2=n%11;
+	printf("%d %d",x1,x2);
 	return 0;
 }
