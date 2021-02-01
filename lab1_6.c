880ad29c60a8bde5007f6dcbb96ce0189d594581
Bowornwat Thongoou <Bowornwat.b@ku.th>
2021-02-01 19:53:22 +0700

    Update4 lab 1_6

--- lab1_6.c
+++ lab1_6.c
@@ -1,48 +1,45 @@
 #include <stdio.h>
 int main()
 {
-	unsigned int n,m;
+	int n,m;
 	unsigned long int t;
 	char s,ans;
 	scanf("%u %u %lu",&n,&m,&t);
 	fflush(stdin);
 	scanf("%c",&s);
-	if(n>0&&m>0&&t>0)
+	if(s=='L')
 	{
-		if(s='L')
+		while(t>0)
 		{
-			while(t>0)
+			t=t-n;
+			if(t<=0)
 			{
-				t=t-n;
-				if(t<=0)
-				{
-					ans='L';
-					break;
-				}
-				t=t-m;
-				if(t<=0)
-				{
-					ans='R';
-					break;
-				}
+				ans='L';
+				break;
+			}
+			t=t-m;
+			if(t<=0)
+			{
+				ans='R';
+				break;
 			}
 		}
-		else if(s='R')
+	}
+	else if(s=='R')
+	{
+		while(t>0)
 		{
-			while(t>0)
+			t=t-m;
+			if(t<=0)
 			{
-				t=t-m;
-				if(t<=0)
-				{
-					ans='R';
-					break;
-				}
-				t=t-n;
-				if(t<=0)
-				{
-					ans='L';
-					break;
-				}
+				ans='R';
+				break;
+			}
+			t=t-n;
+			if(t<=0)
+			{
+				ans='L';
+				break;
 			}
 		}
 	}
